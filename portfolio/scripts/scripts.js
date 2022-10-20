74d3e1dbca6f23f9ebee478a3f42425a2e3af3d1
krube73 <kdogg73@gmail.com>
2022-10-20 13:30:57 -0400

    Add files via upload

---
+++ portfolio/scripts/scripts.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $(".btn[data-id]").on('click', function () {
+        var dataID = $(this).data("id");
+        window.location.href = dataID;
+    });
+
+    $(".navbar > div").on('click', function () {
+        var dataID = $(this).data("id");
+        $(this).toggleClass("active");
+        $(".navbar > div[data-id != '" + dataID + "']").removeClass("active");
+        $(".category[data-id = '" + dataID + "']").slideDown().css({ "display": "flex" });
+        $(".category[data-id != '" + dataID + "']").slideUp().css({ "display": "none" });
+    });
+
+    $(".navbar > div[data-id = 'work']").trigger("click");
+
+    $(".section-head").on('click', function () {
+        var dataID = $(this).data("id")
+        $(this).toggleClass("active");
+        $(".section-head[data-id != '"+dataID+"']").removeClass("active")
+        $(".section-content[data-id = '" + dataID + "']").slideDown();
+        $(".section-content[data-id != '" + dataID + "']").slideUp();
+    });
+});
